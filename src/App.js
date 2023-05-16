0fa6367d42a5001bf9cd855ab6c218e7055db5b0
Stiflerzak <hilarymwendia97@gmail.com>
2023-05-16 12:29:01 +0300

    Added services

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Fragment } from "react";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Services from './components/Services';
+
 
 function App() {
   return <Fragment>
-  
+  <Navbar/>
+  <Hero/>
+  <Services/>
   </Fragment>;
 }
 
