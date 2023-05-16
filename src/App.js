712c7c05b65a71d140497dc247917ced4ecfa9f1
Stiflerzak <hilarymwendia97@gmail.com>
2023-05-16 18:48:10 +0300

    Added links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Fragment } from "react";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
-import Tours from './components/Services';
+import Tours from './components/Tours';
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 
 function App() {
   return <Fragment>
   <Navbar/>
   <Hero/>
+  <About/>
   <Services/>
   <Tours/>
   <Footer/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,66 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="GreenApp" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars"></i>
-          </button>
+    <div>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="GreenApp" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+
+          <ul className="nav-links" id="nav-links">
+            {pageLinks.map((link) => (
+              
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="nav-icons">
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nav-icon"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nav-icon"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nav-icon"
+              >
+                <i className="fab fa-squarespace"></i>
+              </a>
+            </li>
+          </ul>
         </div>
-
-        <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              {" "}
-              home{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              {" "}
-              about{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {" "}
-              services{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {" "}
-              tours
-            </a>
-          </li>
-        </ul>
-
-        <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
+
 export default Navbar;
