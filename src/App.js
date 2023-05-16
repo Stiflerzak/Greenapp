f44c16351a836d6d5a45d07e6042e262d756a6d7
Stiflerzak <hilarymwendia97@gmail.com>
2023-05-16 12:32:05 +0300

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Fragment } from "react";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Tours from './components/Services';
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
   <Navbar/>
   <Hero/>
   <Services/>
+  <Tours/>
+  <Footer/>
+
   </Fragment>;
 }
 
