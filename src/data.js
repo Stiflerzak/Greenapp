6c0a9dc5d2fc7aada969ddd9750fba3802d7f816
Stiflerzak <hilarymwendia97@gmail.com>
2023-05-17 17:37:17 +0300

    Refactored code

--- src/data.js
+++ src/data.js
@@ -14,3 +14,10 @@ export const socialLinks= [
     {id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter'},
     {id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace'},
 ] 
+
+// services data
+export const services= [
+    {id: 1, icon: 'fas fa-wallet fa-fw', title: 'Saving Money', text:'At Greeny, we understand the importance of saving money while making eco-friendly choices. We offer you a unique opportunity to embark on unforgettable travel experiences that not only prioritize adventure and exploration but also help you save money in the long run.'},
+    {id: 2, icon: 'fas fa-tree fa-fw', title: 'Hiking', text:'At Greeny, we believe that hiking is more than just an outdoor activity, its a transformative experience that reconnects us with the natural world. '},
+    {id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', text:'Experience the perfect blend of eco-consciousness and comfort at Greeny, where every detail of our accommodations is carefully crafted to ensure a cozy and environmentally-friendly stay.'},
+] 
